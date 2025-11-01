b22d253f5ac3f383532bba2946f086491d6a1014
Eng-Asad <asadisse12@gmail.com>
2025-11-01 09:58:10 +0300

    Portfolio

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,23 +1,27 @@
+import { CopyIcon, Github } from "lucide-react";
+import { DiGithub } from "react-icons/di";
+import { FaFacebook, FaGithub, FaWhatsapp } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-300 py-6 flex justify-between items-center ">
-      <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row items-center justify-between">
-        <p className="text-sm mb-4 md:mb-0">
-          © {new Date().getFullYear()} Eng-Asad. All rights reserved.
-        </p>
-
-        <div className="flex space-x-6">
-          <a href="#" className="hover:text-primary text-sm">
-            GitHub
-          </a>
-          <a href="#" className="hover:text-primary text-sm">
-            LinkedIn
-          </a>
-          <a href="#" className="hover:text-primary text-sm">
-            Twitter
-          </a>
+    <footer className="bg-gray-300 dark:bg-gray-900 text-gray-900 dark:text-gray-300 py-6 flex justify-between items-center ">
+      <div className="flex flex-col md:flex md:flex-row justify-between items-center mx-40 space-y-10 space-x-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-center">
+        <div>
+          <a href="https://github.com/dashboard"><FaGithub className=" size-16"/></a>
+          <span className="text-2xl font-medium">Github</span>
+        </div>
+        <div>
+          <a href="https://www.facebook.com/asad.ciise.142/"><FaFacebook className=" size-16"/></a>
+          <span className="text-2xl font-medium">Facebook</span>
+        </div>
+        <div>
+          <a href="https://api.whatsapp.com/send?phone=+252610729128&text="><FaWhatsapp className=" size-16 hover:transition-all ease-in-out duration-200 transition-shadow "/></a>
+          <span className=" text-2xl font-medium">WhatsApp</span>
+        </div>
         </div>
       </div>
+
     </footer>
   );
 };
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -40,46 +40,55 @@ const Projects = () => {
       >
         My Projects
       </motion.h2>
-
-      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
-        {projects.map((project, index) => (
-          <motion.div
-            key={index}
-            initial={{ opacity: 0, y: 40 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.7, delay: index * 0.2 }}
-            className="bg-white  rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition cursor-pointer"
-          >
-            <img
-              src={project.img}
-              alt={project.title}
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-6 text-left">
-              <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-              <p className="text-gray-600 dark:text-gray-300 text-sm mb-3">
-                {project.desc}
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {project.tech.map((tech, i) => (
-                  <span
-                    key={i}
-                    className="bg-primary/10 text-primary text-xs font-medium px-2 py-1 rounded-full"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-              <a
-                href={project.link}
-                className="text-primary hover:underline font-semibold"
-              >
-                View Project →
-              </a>
+      <motion.div
+       initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.7 }}
+      className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
+      >
+        {/* Portfolio div */}
+        <div className=" border border-gray-200 shadow-lg cursor-pointer hover:shadow-2xl">
+          <img src={Portfolio} alt="Portfolio " className=" w-full h-48 object-cover" />
+          <h1 className="text-start mx-2 text-2xl font-medium my-4">Portfolio Website</h1>
+          <p className="text-gray-400 font-medium mx-1">A modern Personal  portolio built with react and Tailwind CSS showcasing my work and skills </p>
+          <div className="flex space-x-10 items-center mx-2 my-4">
+            <h1 className="font-medium text-1xl">React</h1>
+            <h1 className="font-medium text-1xl">Taiwlind CSS</h1>
+            <h1 className="font-medium text-1xl">Framer Motion</h1>
+          </div>
+          <div>
+              <button className="items-start text-2xl my-3 border border-gray-400 p-3 cursor-pointer hover:bg-pink-400 font-medium">View Project</button>
             </div>
-          </motion.div>
-        ))}
-      </div>
+        </div>
+        {/* Grocery div */}
+        <div className=" border border-gray-200 shadow-md cursor-pointer hover:shadow-2xl">
+          <img src={Grocery} alt="Portfolio " className=" w-full h-48 object-cover" />
+          <h1 className="text-start mx-2 text-2xl font-medium my-4">Grocery Web App</h1>
+          <p className="text-gray-400 font-medium mx-1">A full-stack grocery app with product management and authentication using MongoDB and Node.Js </p>
+          <div className="flex space-x-10 items-center mx-2 my-4">
+            <h1 className="font-medium text-1xl">Node.Js</h1>
+            <h1 className="font-medium text-1xl">Express</h1>
+            <h1 className="font-medium text-1xl">MongoDB</h1>
+          </div>
+          <div>
+              <button className="items-start text-2xl my-3 border border-gray-400 p-3 cursor-pointer hover:bg-pink-400 font-medium">View Project</button>
+            </div>
+        </div>
+        {/* Mobile App div */}
+        <div className=" border border-gray-200 shadow-lg cursor-pointer hover:shadow-2xl">
+          <img src={Mobile} alt="Portfolio " className=" w-full h-48 object-cover" />
+          <h1 className="text-start mx-2 text-2xl font-medium my-4">Mobile App UI</h1>
+          <p className="text-gray-400 font-medium mx-1">A sleek Mobile App interface designed in Flutter and insepired by modern UI trends </p>
+          <div className="flex space-x-10 items-center mx-2 my-4">
+            <h1 className="font-medium text-1xl">React</h1>
+            <h1 className="font-medium text-1xl">Taiwlind CSS</h1>
+            <h1 className="font-medium text-1xl">Framer Motion</h1>
+          </div>
+          <div>
+              <button className="items-start text-2xl my-3 border border-gray-400 p-3 cursor-pointer hover:bg-pink-400 font-medium">View Project</button>
+            </div>
+        </div>
+      </motion.div>
     </section>
   );
 };
